5ffc0417d61649cda6b3f90d0c49745147ca88a4
Prince Narteh <prinarteh@gmail.com>
2025-12-11 21:04:34 +0000

    feat: minor changes

--- src/app/api/[[...slugs]]/messages.ts
+++ src/app/api/[[...slugs]]/messages.ts
@@ -1,5 +1,7 @@
+import { Message, realtime } from "@/lib/realtime";
 import { redis } from "@/lib/redis";
 import Elysia from "elysia";
+import { nanoid } from "nanoid";
 import { z } from "zod";
 import { authMiddleware } from "./auth";
 
@@ -16,6 +18,17 @@ export const messages = new Elysia({ prefix: "/messages" })
       if (!roomExists) {
         throw new Error("Room does not exists");
       }
+
+      const message: Message = {
+        id: nanoid(),
+        sender,
+        text,
+        roomId,
+        timestamp: Date.now(),
+      };
+
+      await redis.rpush(`message:${roomId}`, { ...message, token: auth.token });
+      await realtime.channel(roomId).emit("chat.message", message);
     },
     {
       query: z.object({ roomId: z.string() }),
